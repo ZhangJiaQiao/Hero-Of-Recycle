6805d088ef5c3e27d507eac685829c5aec3017be
123zzj123 <2893731652@qq.com>
2017-08-09 22:57:30 +0800

    志坚重新创建场景hotel以及实现人物布娃娃系统，血条定位

---
+++ Assets/Resources/Scripts/HeroPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(30, Screen.height - 42, 4));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(30, Screen.height - 42, 4));
+        this.transform.rotation = Camera.main.transform.rotation;
+    }
+}
